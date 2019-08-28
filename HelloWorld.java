053991892e343901d3b7b0439e4bc56da3be0a7f
Andrea <andrea9973@gmail.com>
2019-08-28 14:08:30 -0400

    Modify hello world program with special message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("2.1");
+        System.out.println("2.1. NUTTTTTTT <3");
     }
 
 }
